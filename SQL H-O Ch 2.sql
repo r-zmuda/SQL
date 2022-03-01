6303d98232ed3209ccf93b47a38de263f394cef0
Ryan <39725571+r-zmuda@users.noreply.github.com>
2022-03-01 12:06:33 -0800

    Update SQL H-O Ch 2.sql

--- SQL H-O Ch 2.sql
+++ SQL H-O Ch 2.sql
@@ -1,32 +1,29 @@
-//SQL Hands-On Chapter 2
-//Ryan Zmuda
-
-//Q1
+--Q1
 SELECT * FROM books;
 
-//Q2
+--Q2
 SELECT title FROM books;
 
-//Q3
+--Q3
 SELECT pubid "Publication Date" FROM books;
 
-//Q4
+--Q4
 SELECT customer#, city, state, FROM customers;
 
-//Q5
+--Q5
 SELECT name, contact "Contact Person", phone FROM publisher;
 
-//Q6
+--Q6
 SELECT DISTINCT category FROM books;
 
-//Q7
+--Q7
 SELECT DISTINCT customer# FROM orders;
 
-//Q8
+--Q8
 SELECT category, title FROM books;
 
-//Q9
+--Q9
 SELECT lname || ', ' || fname FROM author;
 
-//Q10
+--Q10
 SELECT order#, item#, isbn, quantity, paideach, quantity*paideach "Item Total" FROM orderitems;
