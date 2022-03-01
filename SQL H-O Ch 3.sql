a676b01b7a45ee460fba501815b6361abf696318
Ryan <39725571+r-zmuda@users.noreply.github.com>
2022-03-01 12:05:52 -0800

    Update SQL H-O Ch 3.sql

--- SQL H-O Ch 3.sql
+++ SQL H-O Ch 3.sql
@@ -1,47 +1,44 @@
-//SQL Hands-On Chapter 3
-//Ryan Zmuda
-
-//Q1
+--Q1
 CREATE TABLE category (
 	catcode CHAR(2),
 	catdesc VARCHAR2(10));
 
-//Q2
+--Q2
 CREATE TABLE employees (
 	emp# NUMBER(5),
 	lastname VARCHAR2(12),
 	firstname VARCHAR2(12),
 	job_class VARCHAR2(4));
 
-//Q3
+--Q3
 ALTER TABLE employees ADD (
 	empdate date DEFAULT SYSDATE,
 	enddate date);
 
-//Q4
+--Q4
 ALTER TABLE employees MODIFY (
 	job_class CHAR(2));
 
-//Q5
+--Q5
 ALTER TABLE employees
 	DROP COLUMN enddate;
 
-//Q6
+--Q6
 RENAME employees TO jl_emps;
 
-//Q7
+--Q7
 CREATE TABLE book_pricing (id, cost, retail, category)
 	AS (SELECT isbn, cost, retail, category FROM books);
 
-//Q8
+--Q8
 ALTER TABLE book_pricing
     SET unused (category);
 SELECT * FROM book_pricing;
 
-//Q9
+--Q9
 TRUNCATE TABLE book_pricing;
 
-//Q10
+--Q10
 DROP TABLE book_pricing purge;
 DROP TABLE jl_emps;
 FLASHBACK TABLE jl_emps TO BEFORE DROP;
